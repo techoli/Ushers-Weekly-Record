81a8ee7460f8676e2714b46b241636003d7b71c6
techoli <okoliemmanuel47@gmail.com>
2025-12-31 13:45:32 +0300

    Updated server2

--- api/db.js
+++ api/db.js
@@ -1,4 +1,4 @@
-// db.js
+// api/db.js
 import mongoose from "mongoose";
 
 let cached = global.mongoose;
@@ -7,20 +7,16 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-export default async function connectToDatabase(uri) {
+export default async function connectToDatabase() {
   if (cached.conn) return cached.conn;
 
+  const MONGO_URI =
+    "mongodb+srv://ushers_admin:Obiagaeli47%40@ushers-cluster.nz4l1zf.mongodb.net/ushers_records?retryWrites=true&w=majority";
+
   if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(
-        "mongodb+srv://ushers_admin:Obiagaeli47%40@ushers-cluster.nz4l1zf.mongodb.net/ushers_records",
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        }
-      )
-      .then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => mongoose);
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
--- api/usersexist.js
+++ api/usersexist.js
@@ -1,19 +1,14 @@
+// api/usersexist.js
 import connectToDatabase from "./db.js";
 import User from "./models/User.js";
 
 export default async function handler(req, res) {
-  console.log(
-    "Mongo URI:",
-    "mongodb+srv://ushers_admin:Obiagaeli47%40@ushers-cluster.nz4l1zf.mongodb.net/ushers_records"
-  );
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
   try {
-    await connectToDatabase(
-      "mongodb+srv://ushers_admin:Obiagaeli47@ushers-cluster.nz4l1zf.mongodb.net/ushers_records"
-    );
+    await connectToDatabase();
 
     const { username, password } = req.body;
 
@@ -24,13 +19,12 @@ export default async function handler(req, res) {
     }
 
     const user = await User.findOne({ username, password });
+
     if (user) {
       return res.status(200).json({ success: true, user });
-    } else {
-      return res
-        .status(200)
-        .json({ success: false, message: "User not found" });
     }
+
+    return res.status(200).json({ success: false, message: "User not found" });
   } catch (err) {
     console.error("Error in /api/usersexist:", err);
     return res.status(500).json({ success: false, message: "Server error" });
